0edafe448a798c5b074cda6e07084e13cd69101b
Uli Heller <uli.heller@daemons-point.com>
2013-04-03 18:31:41 +0200

    New: dumpToList()

--- hexdump/src/main/java/org/uli/hexdump/HexDump.java
+++ hexdump/src/main/java/org/uli/hexdump/HexDump.java
@@ -1,5 +1,8 @@
 package org.uli.hexdump;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class HexDump {
 
     static private final int DEFAULT_BYTES_PER_LINE = 16;
@@ -23,33 +26,39 @@ public class HexDump {
         this.bytesPerLine = bytesPerLine;
     }
 
-    private String dump(byte[] array, int offset, int length) {
+    public String dump(byte[] array) {
+        return this.dump(array, 0, array.length);
+    }
+
+    public String dump(byte[] array, int offset, int length) {
         StringBuffer sb = new StringBuffer(length * 3 + 100);
         int remainingLength = length;
         int thisLineOffset = offset;
         while (remainingLength > 0) {
-            StringBuffer hexBuffer = new StringBuffer(3 * this.bytesPerLine);
-            StringBuffer textBuffer = new StringBuffer(this.bytesPerLine);
-            sb.append(address(thisLineOffset));
-            sb.append(":");
-            int i = 0;
-            for (; i < remainingLength && i < this.bytesPerLine; i++) {
-                byte b = array[thisLineOffset + i];
-                append(hexBuffer, String.format("%02x", b), i);
-                textBuffer.append(text(b));
-            }
-            for (; i < this.bytesPerLine; i++) {
-                append(hexBuffer, "  ", i);
-            }
-            sb.append(hexBuffer);
-            sb.append("  '");
-            sb.append(textBuffer);
-            sb.append("'");
+            dumpLineResult dlr = this.dumpLine(array, thisLineOffset, remainingLength);
+            sb.append(dlr.line);
             sb.append(LINE_SEPARATOR);
-            thisLineOffset += this.bytesPerLine;
-            remainingLength -= this.bytesPerLine;
+            remainingLength = dlr.remainingLength;
+            thisLineOffset = dlr.nextOffset;
         }
         return sb.toString();
+    }
+
+    public List<String> dumpToList(byte[] array) {
+        return this.dumpToList(array, 0, array.length);
+    }
+
+    public List<String> dumpToList(byte[] array, int offset, int length) {
+        List<String> l = new LinkedList<String>();
+        int remainingLength = length;
+        int thisLineOffset = offset;
+        while (remainingLength > 0) {
+            dumpLineResult dlr = this.dumpLine(array, thisLineOffset, remainingLength);
+            l.add(dlr.line);
+            remainingLength = dlr.remainingLength;
+            thisLineOffset = dlr.nextOffset;
+        }
+        return l;
     }
 
     private StringBuffer append(StringBuffer sb, String a, int i) {
@@ -80,5 +89,45 @@ public class HexDump {
 
     public static void main(String[] args) {
         System.out.println(HexDump.hexDump(args[0].getBytes()));
+    }
+
+    private dumpLineResult dumpLine(byte[] array, int offset, int length) {
+        dumpLineResult dlr = new dumpLineResult("", offset, length);
+        StringBuffer sb = new StringBuffer();
+        if (length > 0) {
+            StringBuffer hexBuffer = new StringBuffer(3 * this.bytesPerLine);
+            StringBuffer textBuffer = new StringBuffer(this.bytesPerLine);
+            sb.append(address(offset));
+            sb.append(":");
+            int i = 0;
+            for (; i < length && i < this.bytesPerLine; i++) {
+                byte b = array[offset + i];
+                append(hexBuffer, String.format("%02x", b), i);
+                textBuffer.append(text(b));
+            }
+            for (; i < this.bytesPerLine; i++) {
+                append(hexBuffer, "  ", i);
+            }
+            sb.append(hexBuffer);
+            sb.append("  '");
+            sb.append(textBuffer);
+            sb.append("'");
+            dlr.line       = sb.toString();
+            dlr.nextOffset = offset + this.bytesPerLine;
+            dlr.remainingLength = length - this.bytesPerLine;
+        }
+        return dlr;
+    }
+
+    private static class dumpLineResult {
+        private String line;
+        private int nextOffset;
+        private int remainingLength;
+
+        private dumpLineResult(String line, int nextOffset, int remainingLength) {
+            this.line = line;
+            this.nextOffset = nextOffset;
+            this.remainingLength = remainingLength;
+        }
     }
 }
--- hexdump/src/test/java/org/uli/hexdump/HexDumpTest.java
+++ hexdump/src/test/java/org/uli/hexdump/HexDumpTest.java
@@ -1,61 +1,73 @@
 package org.uli.hexdump;
 
 import static org.junit.Assert.*;
+
+import java.util.List;
+
 import org.junit.Test;
 
 public class HexDumpTest {
+
     @Test
     public void test1() {
-	String one="1";
-	String hexDump=HexDump.hexDump(one.getBytes());
-	assertEquals("00000000: 31                                   '1'"+System.getProperty("line.separator"), hexDump);
+        String one = "1";
+        String hexDump = HexDump.hexDump(one.getBytes());
+        assertEquals("00000000: 31                                   '1'" + System.getProperty("line.separator"), hexDump);
     }
 
     @Test
     public void test12345678() {
-	String oneToEight="12345678";
-	String hexDump=HexDump.hexDump(oneToEight.getBytes());
-	assertEquals("00000000: 31323334 35363738                    '12345678'"+System.getProperty("line.separator"), hexDump);
+        String oneToEight = "12345678";
+        String hexDump = HexDump.hexDump(oneToEight.getBytes());
+        assertEquals("00000000: 31323334 35363738                    '12345678'" + System.getProperty("line.separator"), hexDump);
     }
 
     @Test
     public void test1234567890123456() {
-	String oneToSixteen="1234567890123456";
-	String hexDump=HexDump.hexDump(oneToSixteen.getBytes());
-	assertEquals("00000000: 31323334 35363738 39303132 33343536  '1234567890123456'"+System.getProperty("line.separator"), hexDump);
+        String oneToSixteen = "1234567890123456";
+        String hexDump = HexDump.hexDump(oneToSixteen.getBytes());
+        assertEquals("00000000: 31323334 35363738 39303132 33343536  '1234567890123456'" + System.getProperty("line.separator"), hexDump);
     }
 
     @Test
     public void test123456789012345612345678() {
-	String oneToTwentyFour="123456789012345612345678";
-	String hexDump=HexDump.hexDump(oneToTwentyFour.getBytes());
-	assertEquals("00000000: 31323334 35363738 39303132 33343536  '1234567890123456'"
-                     +System.getProperty("line.separator")
-                     +"00000010: 31323334 35363738                    '12345678'"
-                     +System.getProperty("line.separator"), hexDump);
+        String oneToTwentyFour = "123456789012345612345678";
+        String hexDump = HexDump.hexDump(oneToTwentyFour.getBytes());
+        assertEquals("00000000: 31323334 35363738 39303132 33343536  '1234567890123456'"
+                    + System.getProperty("line.separator")
+                    + "00000010: 31323334 35363738                    '12345678'"
+                    + System.getProperty("line.separator"), hexDump);
     }
 
     @Test
     public void testS0x3f() {
-	String s3f=new String(new byte[]{0x3f});
-	String hexDump=HexDump.hexDump(s3f.getBytes());
-	assertEquals("00000000: 3f                                   '?'"
-                     +System.getProperty("line.separator"), hexDump);
+        String s3f = new String(new byte[] { 0x3f });
+        String hexDump = HexDump.hexDump(s3f.getBytes());
+        assertEquals("00000000: 3f                                   '?'"
+                    + System.getProperty("line.separator"), hexDump);
     }
 
     @Test
     public void testS0x12() {
-	String s12=new String(new byte[]{0x12});
-	String hexDump=HexDump.hexDump(s12.getBytes());
-	assertEquals("00000000: 12                                   '.'"
-                     +System.getProperty("line.separator"), hexDump);
+        String s12 = new String(new byte[] { 0x12 });
+        String hexDump = HexDump.hexDump(s12.getBytes());
+        assertEquals("00000000: 12                                   '.'"
+                    + System.getProperty("line.separator"), hexDump);
     }
 
     @Test
     public void testS0x7f() {
-	String s7f=new String(new byte[]{0x7f});
-	String hexDump=HexDump.hexDump(s7f.getBytes());
-	assertEquals("00000000: 7f                                   '.'"
-                     +System.getProperty("line.separator"), hexDump);
+        String s7f = new String(new byte[] { 0x7f });
+        String hexDump = HexDump.hexDump(s7f.getBytes());
+        assertEquals("00000000: 7f                                   '.'"
+                    + System.getProperty("line.separator"), hexDump);
+    }
+    
+    @Test
+    public void testDumpToList() {
+        String oneToTwentyFour = "123456789012345612345678";
+        List<String> hexDump = new HexDump().dumpToList(oneToTwentyFour.getBytes());
+        assertEquals("00000000: 31323334 35363738 39303132 33343536  '1234567890123456'", hexDump.get(0));
+        assertEquals("00000010: 31323334 35363738                    '12345678'",         hexDump.get(1));
     }
 }
