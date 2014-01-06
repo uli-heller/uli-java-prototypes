6d2db09925dc0267b3772de41bf86459c508d4ad
Uli Heller <uli.heller@daemons-point.com>
2014-01-06 12:06:19 +0100

    jopt-simple: Fixed enum

--- jopt-simple/src/test/java/org/uli/joptsimple/OptionArgumentValueTypeTest.java
+++ jopt-simple/src/test/java/org/uli/joptsimple/OptionArgumentValueTypeTest.java
@@ -25,41 +25,12 @@ public class OptionArgumentValueTypeTest {
         Integer i = (Integer) options.valueOf("count");
         assertEquals( Integer.valueOf( 3 ), options.valueOf( "count" ) );
 
-	/*
         assertTrue( options.has( "level" ) );
         assertTrue( options.hasArgument( "level" ) );
-	Level k = Level.valueOf("INFO");
-        --
-        -- joptsimple.OptionArgumentConversionException: Cannot parse argument 'INFO' of option ['level']
-	--   at joptsimple.AbstractOptionSpec.convertWith(AbstractOptionSpec.java:94)
-	--   at joptsimple.ArgumentAcceptingOptionSpec.convert(ArgumentAcceptingOptionSpec.java:276)
-	--   at joptsimple.OptionSet.valuesOf(OptionSet.java:220)
-	--   at joptsimple.OptionSet.valueOf(OptionSet.java:169)
-	--   at joptsimple.OptionSet.valueOf(OptionSet.java:150)
-	--   at org.uli.joptsimple.OptionArgumentValueTypeTest.convertsArgumentsToJavaValueTypes(OptionArgumentValueTypeTest.java:31)
-	--   at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
-        -- ...
-        -- Caused by: joptsimple.internal.ReflectionException: java.lang.IllegalAccessException: Class joptsimple.internal.Reflection can not access a member of class org.uli.joptsimple.Level with modifiers "public static"
-	--   at joptsimple.internal.Reflection.reflectionException(Reflection.java:144)
-	--   at joptsimple.internal.Reflection.invoke(Reflection.java:122)
-	--   at joptsimple.internal.MethodInvokingValueConverter.convert(MethodInvokingValueConverter.java:48)
-	--   at joptsimple.internal.Reflection.convertWith(Reflection.java:128)
-	--   at joptsimple.AbstractOptionSpec.convertWith(AbstractOptionSpec.java:91)
-	--   ... 47 more
-        -- Caused by: java.lang.IllegalAccessException: Class joptsimple.internal.Reflection can not access a member of class org.uli.joptsimple.Level with modifiers "public static"
-	--   at sun.reflect.Reflection.ensureMemberAccess(Reflection.java:101)
-	--   at java.lang.reflect.AccessibleObject.slowCheckMemberAccess(AccessibleObject.java:295)
-	--   at java.lang.reflect.AccessibleObject.checkAccess(AccessibleObject.java:287)
-	--   at java.lang.reflect.Method.invoke(Method.java:476)
-	--   at joptsimple.internal.Reflection.invoke(Reflection.java:119)
-	--   ... 50 more
-        --
-        Level l = (Level) options.valueOf("level");
         assertEquals( Level.INFO, options.valueOf( "level" ) );
-        */
+    }
+    public enum Level {
+      DEBUG, INFO, ERROR
     }
 }
 
-enum Level {
-    DEBUG, INFO, ERROR
-}
