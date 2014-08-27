0915fedb9b2b1b61ccb269fb01fe82b135fcdd80
Uli Heller <uli.heller@daemons-point.com>
2014-08-27 06:43:22 +0200

    Added jsp

--- spring-boot/README.md
+++ spring-boot/README.md
@@ -1,1 +1,35 @@
-http://spring.io/guides/gs/spring-boot/
+sprint-boot
+===========
+
+Quellen:
+
+* http://spring.io/guides/gs/spring-boot/
+* https://github.com/spring-projects/spring-boot/tree/master/spring-boot-samples/spring-boot-sample-web-jsp
+
+Historie
+--------
+
+* 2014-08-27
+    * Beginn der Historie
+
+Probleme
+--------
+
+### Unable to read TLD "META-INF/c.tld"
+
+Das Problem tritt bei mir immer auf, wenn ich versuche, eine
+JSP im Browser zu laden.
+
+Abhilfe über build.gradle:
+
+```
+...
+apply plugin: 'war'
+...
+  providedCompile("org.springframework.boot:spring-boot-starter-tomcat")
+...
+  providedCompile("org.apache.tomcat.embed:tomcat-embed-jasper")
+...
+```
+
+### Aktivieren des Loggings
--- spring-boot/src/main/java/hello/Application.java
+++ spring-boot/src/main/java/hello/Application.java
@@ -15,17 +15,17 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 @ComponentScan
 public class Application extends WebMvcConfigurerAdapter {
 
-    public static void main(String[] args) {
-            ApplicationContext ctx = SpringApplication.run(Application.class, args);
-            
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
-                    
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                 System.out.println(beanName);
-            }
-    }
+    //public static void main(String[] args) {
+    //        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+    //        
+    //        System.out.println("Let's inspect the beans provided by Spring Boot:");
+    //                
+    //        String[] beanNames = ctx.getBeanDefinitionNames();
+    //        Arrays.sort(beanNames);
+    //        for (String beanName : beanNames) {
+    //             System.out.println(beanName);
+    //        }
+    //}
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
