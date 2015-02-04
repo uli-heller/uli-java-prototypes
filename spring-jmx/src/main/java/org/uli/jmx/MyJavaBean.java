a858180041b290790409108e0a6977d01c498361
Uli Heller <uli.heller@daemons-point.com>
2015-02-04 07:38:03 +0100

    Added a java bean

---
+++ spring-jmx/src/main/java/org/uli/jmx/MyJavaBean.java
@@ -0,0 +1,12 @@
+package org.uli.jmx;
+
+import java.math.BigDecimal;
+
+import lombok.Data;
+
+@Data
+public class MyJavaBean {
+    Integer integer;
+    BigDecimal bigDecimal;
+    String string;
+}
